d0d7b1006eb9121c9b08c108063a8e8d8f2a2a82
Ellie Diaz <elliediaz@Ellies-Air.lan>
2022-05-19 19:22:00 -0500

    show route connected"

--- controllers/places.js
+++ controllers/places.js
@@ -21,9 +21,19 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-// router.get('/id', (req, res) => {
-//   res.render('places/:id')
-// })
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNAN(id)) {
+    res.render('error404')
+  } 
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else{
+    res.render('places/show', { places: places[id] })
+  }
+})
 
 module.exports = router
 
