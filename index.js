45f7807369d2ab93b775c16c83c3baf97ec60ec3
Ellie Diaz <elliediaz@Ellies-Air.lan>
2022-05-20 17:56:42 -0500

    Added the edit functionality

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-//Express Settings 
+//Express Settings (middleware)
 app.set ('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
